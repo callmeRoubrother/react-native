95f7820b136171d5b74e50c2bdd4d8c8e68d9363
wangxinlong <17607159818@163.com>
2018-12-05 11:29:26 +0800

    首页基本完成

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet, Dimensions, ActivityIndicator, FlatList, TouchableHighlight } from 'react-native';
+import { View, Text, Image, StyleSheet, Dimensions, ActivityIndicator, FlatList, TouchableHighlight, Platform } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import Swiper from 'react-native-swiper';
+import { createStackNavigator } from 'react-navigation';
+import Detail from '../detail';
 
 const { width, height } = Dimensions.get('window');
-export default class Com extends React.Component {
+class Com extends React.Component {
+  static navigationOptions = {
+    title: '首页'
+  }
   constructor (props) {
     super(props);
     this.state = {
@@ -11,6 +17,7 @@ export default class Com extends React.Component {
       bannerList: [],
       listPage: 1,
       girlsList: [],
+      showBackTop: false,
     }
   }
   renderSwiper (arr) {
@@ -52,49 +59,138 @@ export default class Com extends React.Component {
       })
     })
   }
-  fetchListData () {
-    const List_URl = 'http://route.showapi.com/126-2?showapi_appid=79073&showapi_sign=1fbe0cb4957a45b5a40ce20d5be0a18e&page=' + this.state.listPage;
+  fetchListData (page) {
+    page = page || 1;
+    const List_URl = 'http://route.showapi.com/126-2?showapi_appid=79073&showapi_sign=1fbe0cb4957a45b5a40ce20d5be0a18e&page=' + page;
     fetch(List_URl).then(res => res.json()).then(data => {
-      console.log(data.showapi_res_body.pagebean.contentlist)
       this.setState({
-        girlsList: data.showapi_res_body.pagebean.contentlist,
+        girlsList: [...this.state.girlsList, ...data.showapi_res_body.pagebean.contentlist],
       })
+      console.log(this.state.girlsList)
     })
   }
   componentDidMount () {
     this.fetchBannerData();
-    this.fetchListData()
+    this.reFresh();
+  }
+  _press () {
+    this.props.navigation.navigate('Details')
+  }
+  hideUnder () {
+    console.log('底层的颜色被隐藏');
+  }
+  showUnder () {
+    console.log('底层的颜色被显示');
+  }
+  endPushList () {
+    this.state.listPage += 1;
+    this.fetchListData(this.state.listPage);
+    this.setState({
+      showBackTop: true,
+    })
+  }
+  reFresh () {
+    this.fetchListData();
+  }
+  backTop () {
+    this.refs.list.scrollToIndex({
+      index: 0,
+    });
+    this.setState({
+      showBackTop: false,
+    })
   }
   render () {
-    if (this.state.bannerList.length > 0) {
+    if (this.state.bannerList.length > 0 && this.state.girlsList.length > 0) {
       return (
         <View style={styles.container}>
-          {
-            this.renderSwiper(this.state.bannerList)
-          }
-          <Text>nihao</Text>
+          <View style={styles.banner_Container}>
+            {
+              this.renderSwiper(this.state.bannerList)
+            }
+          </View>
+          <View style={{ flex: 1 }}>
+            <FlatList
+              ref='list'
+              ItemSeparatorComponent={() => <View style={styles.separator} />}
+              data={this.state.girlsList}
+              onEndReachedThreshold={0.4}
+              refreshing={false}
+              onRefresh={() => {this.reFresh()}}
+              onEndReached={(info: {distanceFromEnd: 100}) => this.endPushList()}
+              renderItem={({item}) => {
+                return (
+                  <TouchableHighlight
+                    onPress={() => this._press()}
+                    activeOpacity={ 0.5 }
+                    underlayColor='gray'
+                    onHideUnderlay={this.hideUnder}
+                    onShowUnderlay={this.showUnder}
+                    style={{ width: width, height: 120 }}
+                  >
+                    <View style={styles.list_cover}>
+                      <Image source={{ uri: item.avatarUrl }} style={{ height: 100, width: 200 }}></Image>
+                      <View style={styles.list_desc}>
+                        <Text>姓名： { item.realName }</Text>
+                        <Text>城市： { item.city }</Text>
+                        <Text>粉丝数：{ item.totalFanNum }</Text>
+                        <Text>身高：{ item.height }</Text>
+                        <Text>体重：{ item.weight }</Text>
+                      </View>
+                    </View>
+                  </TouchableHighlight>
+                )
+              }}
+            ></FlatList>
+            {
+              this.state.showBackTop === true? (<Ionicons
+                name={'ios-arrow-dropup-circle'}
+                size={ 50 }
+                style={styles.upTop}
+                onPress={this.backTop.bind(this)}
+                ref='backTop'
+              />) : (false)
+            }
+          </View>
         </View>
       )
     } else {
       return (
-        <View style={styles.container}>
-          <ActivityIndicator size="large" color="#0000ff" />
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <ActivityIndicator size="large" color="#0000ff" style={{ alignSelf: "center" }}/>
         </View>
       )
     }
   }
 }
 
+const RootStack = createStackNavigator(
+  {
+    Home: Com,
+    Details: Detail,
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+export default class App extends React.Component {
+  render() {
+    return <RootStack />;
+  }
+}
+
 const styles = StyleSheet.create({
   container: {
-    height:width * 40 / 75,
+    flex: 1,
+  },
+  banner_Container: {
+    height:width * 40 / 100,
   },
   wrpaper: {
     width: width,
-    height:width * 40 / 50,
+    height:width * 40 / 100,
   },
   bannerImg: {
-    marginTop: 44,
     width: width,
     height:width * 40 / 100,
   },
@@ -113,5 +209,26 @@ const styles = StyleSheet.create({
     height: 3,
     backgroundColor: '#fff',
     borderRadius: 0,
+  },
+  list_cover: {
+    flexDirection: 'row',
+    width: width,
+    height: 120,
+    padding: 10,
+  },
+  list_desc: {
+    paddingLeft: 10,
+    textAlign: 'left',
+    lineHeight: 10,
+  },
+  separator: {
+    width: width,
+    height: 1,
+    backgroundColor: 'gray',
+  },
+  upTop: {
+    position:'absolute',
+    bottom: 10,
+    right: 10,
   },
 });
